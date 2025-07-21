849d825e8945a2bc1cb6c0de088b7f3df076beae
kyeongho <kyeongho_jo@lotte.net>
2025-07-21 14:45:21 +0900

    [FE] #6 서버 목록 페이지 내 컴포넌트 연동
    - 서버 목록 라우팅 페이지 내 ServerList 컴포넌트 연동
    - 기존 디자인 시안에 따라 일부 화면 구성

--- cloudtype-manager-ui/src/app/servers/page.tsx
+++ cloudtype-manager-ui/src/app/servers/page.tsx
@@ -1,9 +1,23 @@
 /* 서버 목록 페이지 */
+import {Plus} from "lucide-react";
+import ServerList from "@/components/server-list";
+
 export default function ServerListPage() {
     return (
-        /* TODO 컴포넌트 구현 예정 */
-        <div className="flex flex-col items-center justify-center w-full">
-            <h1>Server List Page</h1>
+        <div className='bg-slate-800 text-white animate-slide-in'>
+            <div className='pt-16 pb-20'>
+                {/* 상단 부분 */}
+                <div className='flex items-center justify-between px-6 py-4'>
+                    <h1 className='text-2xl font-bold'>Servers</h1>
+                    <button
+                        className='p-2 hover:bg-slate-700 rounded-lg transition-all duration-200 hover:scale-105 active:scale-95'>
+                        <Plus className='w-6 h-6'/>
+                    </button>
+                </div>
+
+                {/* 서버 목록 */}
+                <ServerList/>
+            </div>
         </div>
     )
 }
