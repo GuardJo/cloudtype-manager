7473251a5f5e153cec4d020e7a730b34fab3b890
kyeongho <kyeongho_jo@lotte.net>
2025-08-04 15:53:16 +0900

    [FE] #8 서버 상세 정보 페이지 구성
    - /servers/{id} 라우팅에 해당하는 서버 상세 정보 페이지 구성
    - ServerDetailContent 컴포넌트 연동

--- cloudtype-manager-ui/src/app/servers/[serverId]/page.tsx
+++ cloudtype-manager-ui/src/app/servers/[serverId]/page.tsx
@@ -1,11 +1,12 @@
+import ServerDetailContent from "@/components/server-detail-content";
+
 /* 서버 상세 정보 페이지 */
 export default async function ServerDetailPage({params}: { params: Promise<{ serverId: number }> }) {
     const {serverId} = await params
 
     return (
-        /* TODO 컴포넌트 구현 예정 */
-        <div className="flex flex-col items-center justify-center w-full">
-            <h1>Server(id = {serverId}) Detail Page</h1>
+        <div className='min-h-screen bg-slate-800 text-white animate-slide-in'>
+            <ServerDetailContent serverId={serverId}/>
         </div>
     )
 }
