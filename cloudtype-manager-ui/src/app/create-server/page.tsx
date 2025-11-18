771cf6de0bcc8a61c5907345dc40ca99346f36eb
Kyeongho <dagh1218@naver.com>
2025-11-18 23:15:01 +0900

    [FE] #65 Mock Server 구성 전 API 호출 제어
    - mock server 모드일 때 mock server가 초기화되기전에 API가 호출되는 것을 막기 위해 mock 구성완료시 까지 지연 처리
    - 미사용 임시 컴포넌트 제거

--- cloudtype-manager-ui/src/app/create-server/page.tsx
+++
@@ -1,9 +0,0 @@
-/* 서버 생성 폼 페이지 */
-export default function CreateServerPage() {
-    return (
-        /* TODO 컴포넌트 구현 예정 */
-        <div className="flex flex-col items-center justify-center w-full">
-            <h1>Create Server Page</h1>
-        </div>
-    )
-}
--- cloudtype-manager-ui/src/containers/mock-provider.tsx
+++ cloudtype-manager-ui/src/containers/mock-provider.tsx
@@ -1,14 +1,34 @@
 'use client'
 
 import {initMocks} from "@/mocks";
-import {ReactNode, useEffect} from "react";
+import {ReactNode, useEffect, useState} from "react";
 
 export default function MockProvider({children}: { children: ReactNode }) {
+    const [isReady, setIsReady] = useState(false)
+
     useEffect(() => {
-        if (process.env.NEXT_PUBLIC_MOCK_SERVER_ENABLE === 'true') {
-            initMocks()
-                .then(() => console.log('mock server is enabled'))
-        }
+        const init = async () => {
+            if (process.env.NEXT_PUBLIC_MOCK_SERVER_ENABLE === 'true') {
+                try {
+                    await initMocks()
+                    console.log('mock server is enabled')
+                } catch (error) {
+                    console.log('Failed initialize mock server', error);
+                }
+            }
+            setIsReady(true)
+        };
+
+        init();
     }, []);
+
+    if (!isReady) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"/>
+            </div>
+        );
+    }
+
     return <>{children}</>
 }
