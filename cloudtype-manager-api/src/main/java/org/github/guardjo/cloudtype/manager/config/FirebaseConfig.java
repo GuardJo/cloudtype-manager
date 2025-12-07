11b98fafc7ce8befac048c73e024b617374339cc
Kyeongho <dagh1218@naver.com>
2025-12-07 20:43:13 +0900

    [BE] #38 firebase-account json 파일 조회 방식 변경
    - classPathStream 방식이 아닌 ResourceLoader를 통한 조회 방식으로 변경
        - ResourceLoader를 통해 file 기반 읽기와 classpath 기반 읽기 모두 지원하도록 함

--- cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/config/FirebaseConfig.java
+++ cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/config/FirebaseConfig.java
@@ -5,21 +5,37 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.github.guardjo.cloudtype.manager.config.properties.FirebaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class FirebaseConfig {
     private final FirebaseProperties firebaseProperties;
+    private final ResourceLoader resourceLoader;
 
     @Bean
-    public FirebaseMessaging firebaseMessaging() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource(firebaseProperties.credentialsPath()).getInputStream());
+    public FirebaseMessaging firebaseMessaging() {
+        GoogleCredentials credentials = null;
+        try {
+            Resource resource = resourceLoader.getResource(firebaseProperties.credentialsPath());
+
+            try (InputStream credentialStream = resource.getInputStream()) {
+                credentials = GoogleCredentials.fromStream(credentialStream);
+            }
+        } catch (IOException e) {
+            log.error("Failed load FirebaseCredentials, cause = {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
                 .build();
