6ae6c036147be73fe4e9f418a83cd2bd0cea05e4
Kyeongho <dagh1218@naver.com>
2026-02-21 20:43:49 +0900

    [BE] #41 특정 회원의 서버별 상태 변경 이력 목록 조회 DAO 구성
    - username 기반 해당하는 계정과 연관관계를 지닌 서버에 대한 서버 상태 변경 이력을 페이지네이션으로 조회하도록 구성
    - 관련 테스트 시나리오 추가
        - 일부 테스트 소스 내 변수 명칭 개선

--- cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/repository/ServerStatusChangeHistoryEntityRepository.java
+++ cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/repository/ServerStatusChangeHistoryEntityRepository.java
@@ -1,7 +1,17 @@
 package org.github.guardjo.cloudtype.manager.repository;
 
 import org.github.guardjo.cloudtype.manager.model.domain.ServerStatusChangeHistoryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServerStatusChangeHistoryEntityRepository extends JpaRepository<ServerStatusChangeHistoryEntity, Long> {
+    /**
+     * 특정 회원이 지닌 server_info 들에 대한 server_status_change_history 목록 조회
+     *
+     * @param username 회원 식별키
+     * @param pageable 페이지네이션
+     * @return 페이지네이션 처리된 server_status_change_history 목록
+     */
+    Page<ServerStatusChangeHistoryEntity> findAllByServer_UserInfo_Username(String username, Pageable pageable);
 }
--- cloudtype-manager-api/src/test/java/org/github/guardjo/cloudtype/manager/repository/ServerStatusChangeHistoryEntityRepositoryTest.java
+++ cloudtype-manager-api/src/test/java/org/github/guardjo/cloudtype/manager/repository/ServerStatusChangeHistoryEntityRepositoryTest.java
@@ -8,8 +8,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +22,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class ServerStatusChangeHistoryEntityRepositoryTest {
+    private final static Logger log = LoggerFactory.getLogger(ServerStatusChangeHistoryEntityRepositoryTest.class);
+
     private final UserInfoEntity testUserInfo = TestDataGenerator.userInfoEntity("tester");
-    private final List<ServerStatusChangeHistoryEntity> serverhealthCheckRunList = new ArrayList<>();
+    private final List<ServerStatusChangeHistoryEntity> serverStatusChangeHistories = new ArrayList<>();
 
     @Autowired
     private UserInfoEntityRepository userInfoRepository;
@@ -28,24 +34,24 @@ class ServerStatusChangeHistoryEntityRepositoryTest {
     private ServerInfoEntityRepository serverInfoRepository;
 
     @Autowired
-    private ServerStatusChangeHistoryEntityRepository serverHealthCheckRunRepository;
+    private ServerStatusChangeHistoryEntityRepository serverStatusChangeHistoryRepository;
 
     @BeforeEach
     void setUp() {
         userInfoRepository.save(testUserInfo);
         ServerInfoEntity testServer = initServerInfo(testUserInfo, "Test Server");
-        ServerStatusChangeHistoryEntity serverStatusChangeHistoryEntity = serverHealthCheckRunRepository.save(ServerStatusChangeHistoryEntity.builder()
+        ServerStatusChangeHistoryEntity serverStatusChangeHistoryEntity = serverStatusChangeHistoryRepository.save(ServerStatusChangeHistoryEntity.builder()
                 .server(testServer)
                 .statusCode(400)
                 .errorCategory("BAD_REQUEST")
                 .build());
-        serverhealthCheckRunList.add(serverStatusChangeHistoryEntity);
+        serverStatusChangeHistories.add(serverStatusChangeHistoryEntity);
     }
 
     @AfterEach
     void tearDown() {
-        serverHealthCheckRunRepository.deleteAll();
-        serverhealthCheckRunList.clear();
+        serverStatusChangeHistoryRepository.deleteAll();
+        serverStatusChangeHistories.clear();
         serverInfoRepository.deleteAll();
         userInfoRepository.deleteAll();
     }
@@ -57,15 +63,52 @@ class ServerStatusChangeHistoryEntityRepositoryTest {
         return testServer;
     }
 
-    @DisplayName("특정 ServerHealthCheckRunEntity 조회")
+    @DisplayName("특정 ServerStatusChangeHistoryEntity 조회")
     @Test
     void test_findById() {
-        ServerStatusChangeHistoryEntity expected = serverhealthCheckRunList.get(0);
+        ServerStatusChangeHistoryEntity expected = serverStatusChangeHistories.get(0);
         Long id = expected.getId();
 
-        ServerStatusChangeHistoryEntity actual = serverHealthCheckRunRepository.findById(id).orElseThrow();
+        ServerStatusChangeHistoryEntity actual = serverStatusChangeHistoryRepository.findById(id).orElseThrow();
 
         assertThat(actual).isNotNull();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("특정 회원이 지닌 서버의 ServerStatusChangeHistoryEntity 목록 페이지네이션 조회")
+    @Test
+    void test_findAllByServer_UserInfo_Username() {
+        log.info("test_findAllByServer_UserInfo_Username: Start");
+        UserInfoEntity userInfo = TestDataGenerator.userInfoEntity("test2");
+        userInfoRepository.save(userInfo);
+
+        ServerInfoEntity serverInfo = TestDataGenerator.serverInfoEntity("test-server2", userInfo);
+        serverInfoRepository.save(serverInfo);
+
+        ServerStatusChangeHistoryEntity serverStatusChangeHistory = ServerStatusChangeHistoryEntity.builder()
+                .server(serverInfo)
+                .statusCode(400)
+                .errorCategory("BAD_REQUEST")
+                .build();
+
+        serverStatusChangeHistoryRepository.save(serverStatusChangeHistory);
+
+        long totalHistoryCount = serverStatusChangeHistoryRepository.count();
+
+        assertThat(totalHistoryCount).isEqualTo(serverStatusChangeHistories.size() + 1);
+
+        String testUserId = userInfo.getUsername();
+        log.info("findAllByServer_UserInfo_Username()1, testUserId = {}, page = {}, size = {}", testUserId, 0, 10);
+        Page<ServerStatusChangeHistoryEntity> actual = serverStatusChangeHistoryRepository.findAllByServer_UserInfo_Username(testUserId, PageRequest.of(0, 10));
+
+        assertThat(actual.isEmpty()).isEqualTo(false);
+        assertThat(actual.getTotalPages()).isEqualTo(1);
+        assertThat(actual.getTotalElements()).isEqualTo(1L);
+        assertThat(actual.getContent().get(0)).isEqualTo(serverStatusChangeHistory);
+
+        log.info("findAllByServer_UserInfo_Username()2, testUserId = {}, page = {}, size = {}", testUserId, 1, 10);
+        actual = serverStatusChangeHistoryRepository.findAllByServer_UserInfo_Username(testUserId, PageRequest.of(1, 10));
+        assertThat(actual.isEmpty()).isEqualTo(true);
+        log.info("test_findAllByServer_UserInfo_Username: Finished");
     }
 }
