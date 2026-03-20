4ba0df08909a9996f5da0d552cecfd2820496826
kyeongho <kyeongho_jo@lotte.net>
2026-03-20 10:48:08 +0900

    [BE] #77 고객문의 발송 API 인터페이스 일부 수정
    - 메일 발송이 아닌 DB에 저장하는 방식으로 변경함에 따라 description 수정

--- cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/controller/UserApiDoc.java
+++ cloudtype-manager-api/src/main/java/org/github/guardjo/cloudtype/manager/controller/UserApiDoc.java
@@ -16,6 +16,6 @@ public interface UserApiDoc {
     @Operation(summary = "앱푸시 토큰 조회", description = "현재 회원 당사자의 앱푸시 토큰을 반환한다.")
     BaseResponse<String> getMyFCMToken(@Parameter(hidden = true) UserInfoPrincipal principal, @Parameter(description = "디바이스 식별키", required = true) String deviceId);
 
-    @Operation(summary = "고객 문의 메일 발송", description = "현재 회원이 작성한 고객문의 사항을 메일로 송신한다.")
+    @Operation(summary = "고객 문의 발송", description = "현재 회원이 작성한 고객문의 사항을 발송한다.")
     BaseResponse<String> sendInquiry(@Parameter(hidden = true) UserInfoPrincipal principal, CustomerInquiryRequest inquiryRequest);
 }
